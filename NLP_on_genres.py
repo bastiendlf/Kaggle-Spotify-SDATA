c641825cca9db55cb588bc931924aee0685d99a4
Bastien DELFORGE <bastien.delforge@etu.imt-lille-douai.fr>
2021-01-19 13:55:34 +0100

    Adding .gitignore Implementing NLP_on_genres.py Vizualisation data exo2 in notebook

---
+++ NLP_on_genres.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def is_there_nan_values(df):
+    """
+    Check if there are NaN values in Dataframe
+    :param df: Dataframe to analyze
+    :return: True if NaN values encounted else False
+    """
+    return False if np.sum(df.isnull().sum()) == 0 else True
+
+
+def getKeysByValue(dictOfElements, valueToFind):
+    """
+    Return the keys in a dictionary corresponding the values given in parameters
+    :param dictOfElements: dictionary to analyze
+    :param valueToFind: value to find corresponding IDs
+    :return: list of corresponding IDs
+    """
+
+    listOfKeys = list()
+    listOfItems = dictOfElements.items()
+    for item in listOfItems:
+        if item[1] == valueToFind:
+            listOfKeys.append(item[0])
+    return listOfKeys
+
+
+def transform_genres(df, target="popularity"):
+    """
+    TODO
+    :param df:
+    :param target:
+    :return:
+    """
+    if is_there_nan_values(df):
+        raise Exception("Sorry, please remove NaN values before transforming Dataframe")
+
+    # We have to remove this genre string '[]' because the string encoder considers it as an empty array
+    # and it raises an Exception so we drop the line to avoid issues
+    df = df[df.genres != '[]']
+
+    columns = df.columns.values
+    features = np.delete(columns, np.argwhere(columns == target))
+
+    genres = np.array(df.genres)
+
+    # lexicon will contain each words in "genres" column
+    lexicon = list()
+
+    for genre in genres:
+        for mot in genre.split(sep=' '):
+            lexicon.append(mot)
+
+    lexicon = np.array(lexicon)
+    # we keep one time each unique word
+    unique = np.unique(lexicon)
+
+    vectorizer = CountVectorizer(strip_accents='unicode')
+
+    # tokenize and build vocab
+    vectorizer.fit(unique)
+
+    transformation = vectorizer.transform(genres).toarray()
+
+    col_names = list()
+    for i in range(len(vectorizer.vocabulary_)):
+        col_names.append(getKeysByValue(vectorizer.vocabulary_, i)[0])
+
+    # Creation of a new dataframe with genre encoded in columns
+    df_genres = pd.DataFrame(transformation, columns=col_names)
+
+    # Adding column "genres" to merge after with the dataframe df
+    df_genres.insert(0, "genres", genres)
+
+    # Merging the original datafram and the new one created
+    df_final = df.merge(df_genres, on="genres", how='inner')
+
+    # df_final = df_final.drop("genres", axis=1)
+
+    return df_final
--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,8 @@ from scipy.stats import zscore
 from sklearn.preprocessing import scale
 import numpy as np
 
-def preprocess_data(df, features_columns, label = None, z_score = False, standardize = False) :
+
+def preprocess_data(df, features_columns, label=None, z_score=False, standardize=False):
     print("------------------------------------------")
     print("            Preprocessing data            ")
     print("------------------------------------------")
@@ -12,46 +13,44 @@ def preprocess_data(df, features_columns, label = None, z_score = False, standar
     print("Shape of the data to process : " + str(df.shape))
     print("------------------------------------------")
 
-    #Create inputs and labels
-    #label
-    if label !=None :
+    # Create inputs and labels
+    # label
+    if label != None:
         print("Extract labels ...")
         df_labels = df['genre']
         print("Encode labels ...")
         le = LabelEncoder()
         df_labels = le.fit_transform(df_labels)
 
-    #inputs
+    # inputs
     print("Extract inputs ...")
     df = df[features_columns]
-    #Remove outliers
-    if z_score :
+    # Remove outliers
+    if z_score:
         print("Remove outliers with zscore ...")
         z_scores = zscore(df)
         abs_z_scores = np.abs(z_scores)
         filtered_entries = (abs_z_scores < 4).all(axis=1)
         df = df[filtered_entries]
-        if label != None : df_labels = df_labels[filtered_entries]
+        if label != None: df_labels = df_labels[filtered_entries]
 
-    #Strandardize : center reduce
-    if standardize :
+    # Strandardize : center reduce
+    if standardize:
         print("Center and reduce inputs ...")
         df = scale(df, axis=0, with_mean=True, with_std=True)
         df = pd.DataFrame(df, columns=features_columns)
 
     print("------------------------------------------")
     print("Data shape after preprocessing : " + str(df.shape))
-    if label != None : print("Labels shape : " + str(df_labels.shape))
+    if label != None: print("Labels shape : " + str(df_labels.shape))
 
     print("Return dataset(s) ...")
     print("Preprocessing finished")
     print("------------------------------------------")
 
-
-    if label != None :
+    if label != None:
         res = (df, df_labels)
-    else :
+    else:
         res = df
 
     return res
-
