686dfb25b475abfd920c53aca5f0ed8df796d657
Bastien DELFORGE <bastien.delforge@etu.imt-lille-douai.fr>
2021-01-23 17:02:59 +0100

    final adjustment

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Projets de groupe 'Les Wigolos'
 
 Bastien Delforge
 Adrien Malgowski
+
+
+Please before running the project, place all the datasets in a folder called /data at the project's root.
--- notebooks_classification/classification.py
+++ notebooks_classification/classification.py
@@ -15,4 +15,23 @@ def compute_classifier(x_train, y_train, model):
     print("Accuracy : " + str(accuracy))
     return y_pred
 
+def make_classification_report(X, y, class_names, clf):
+    kfold = KFold(n_splits=10, random_state=5)
+    y_pred = cross_val_predict(clf, X, y, cv=kfold)
+
+    cf_matrix = confusion_matrix(y, y_pred)
+
+    fig, ax = plt.subplots(figsize=(15,15))
+
+    sns.heatmap(cf_matrix, annot=True, ax=ax, cmap='hot_r')
+    plt.show()
+    return classification_report(y, y_pred, target_names=class_names)
+
+def plot_scores_vs_depth(depth_list, score_list, title):
+    plt.plot(depth_list, score_list, label='F1-Score vs depth')
+    plt.xlabel('Depth')
+    plt.suptitle(title)
+    plt.grid()
+    plt.show()
+
 
